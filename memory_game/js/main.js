d1f555547db81d6301898459095b010d02172105
Kenneth Chua <kenneththesheep@gmail.com>
2020-01-30 15:35:05 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,29 @@
 //console.log("Up and running!");
-let cardOne="queen";
+/*let cardOne="queen";
 let cardTwo="queen";
 let cardThree="king";
 let cardFour="king";
 console.log("User flipped "+cardOne);
-console.log("User flipped "+cardThree);
+console.log("User flipped "+cardThree);*/
+let cards=["queen", "queen", "king", "king"]
+cardsInPlay=[];
+let cardOne;
+cardOne=cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " +cardsInPlay[cardsInPlay.length-1]);
+let cardTwo;
+cardTwo=cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " +cardsInPlay[cardsInPlay.length-1]);
+if(cardsInPlay.length===2)
+{
+	if(cardsInPlay[0]===cardsInPlay[cardsInPlay.length-1])
+	{
+		alert("You found a match!");
+	}else
+	{
+			alert("Try again!");
+	}
+
+
+}
