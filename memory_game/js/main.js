357483d2e94140741d4260ee0cd5520c9d21dc6e
Kenneth Chua <kenneththesheep@gmail.com>
2020-01-30 16:23:56 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,17 +4,7 @@ let cardTwo="queen";
 let cardThree="king";
 let cardFour="king";
 console.log("User flipped "+cardOne);
-console.log("User flipped "+cardThree);*/
-let cards=["queen", "queen", "king", "king"]
-cardsInPlay=[];
-let cardOne;
-cardOne=cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " +cardsInPlay[cardsInPlay.length-1]);
-let cardTwo;
-cardTwo=cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " +cardsInPlay[cardsInPlay.length-1]);
+console.log("User flipped "+cardThree);
 if(cardsInPlay.length===2)
 {
 	if(cardsInPlay[0]===cardsInPlay[cardsInPlay.length-1])
@@ -26,4 +16,42 @@ if(cardsInPlay.length===2)
 	}
 
 
+}*/
+let cards=["queen", "queen", "king", "king"]
+let cardsInPlay=[];
+//function to check for match
+function checkForMatch()
+{
+	if(cardsInPlay.length<2)
+	{
+
+	}
+	else{
+		if(cardsInPlay[0]===cardsInPlay[1])
+		{
+			console.log("You found a match!");
+		}else
+		{
+			console.log("Try again!");
+		}
+		}		
 }
+//function to check which card is picked
+function flipCard(cardID)
+{
+	console.log("User flipped "+ cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+	
+	/*let cardOne=cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	let cardTwo=cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);*/
+
+}
+
+flipCard(0);
+flipCard(2);
